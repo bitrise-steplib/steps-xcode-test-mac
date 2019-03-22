dc018e33163d8980aea2b4dcc3936d3c7855f39c
Akos Birmacher <37296013+BirmacherAkos@users.noreply.github.com>
2019-03-22 11:05:41 +0100

    Maintenance (#13)
    * Update bitrise.yml
    
    * update ConfigsModel to use the new stepconf parser and make it private
    
    * add test workflow for the xcodebuildoptions

--- main.go
+++ main.go
@@ -5,119 +5,29 @@ import (
 	"os"
 	"strings"
 
-	"errors"
-
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/log"
-	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/go-utils/stringutil"
+	"github.com/bitrise-tools/go-steputils/stepconf"
 	"github.com/bitrise-tools/go-xcode/utility"
 	"github.com/bitrise-tools/go-xcode/xcodebuild"
 	"github.com/bitrise-tools/go-xcode/xcpretty"
 	"github.com/kballard/go-shellquote"
 )
 
-// ConfigsModel ...
-type ConfigsModel struct {
+// configs ...
+type configs struct {
 	// Project parameters
-	ProjectPath string
-	Scheme      string
-	Destination string
+	ProjectPath string `env:"project_path,dir"`
+	Scheme      string `env:"scheme,required"`
+	Destination string `env:"destination"`
 
 	// Test Run Configs
-	OutputTool   string
-	IsCleanBuild string
+	OutputTool   string `env:"output_tool,opt[xcpretty,xcodebuild]"`
+	IsCleanBuild bool   `env:"is_clean_build,opt[yes,no]"`
 
-	GenerateCodeCoverageFiles string
-	XcodebuildOptions         string
-}
-
-func (configs ConfigsModel) print() {
-	fmt.Println()
-	log.Infof("Project Parameters:")
-	log.Printf("- ProjectPath: %s", configs.ProjectPath)
-	log.Printf("- Scheme: %s", configs.Scheme)
-	log.Printf("- Destination: %s", configs.Destination)
-
-	fmt.Println()
-	log.Infof("Test Run Configs:")
-	log.Printf("- OutputTool: %s", configs.OutputTool)
-	log.Printf("- IsCleanBuild: %s", configs.IsCleanBuild)
-
-	log.Printf("- GenerateCodeCoverageFiles: %s", configs.GenerateCodeCoverageFiles)
-	log.Printf("- XcodebuildOptions: %s", configs.XcodebuildOptions)
-}
-
-func createConfigsModelFromEnvs() ConfigsModel {
-	return ConfigsModel{
-		// Project Parameters
-		ProjectPath: os.Getenv("project_path"),
-		Scheme:      os.Getenv("scheme"),
-		Destination: os.Getenv("destination"),
-
-		// Test Run Configs
-		OutputTool:   os.Getenv("output_tool"),
-		IsCleanBuild: os.Getenv("is_clean_build"),
-
-		GenerateCodeCoverageFiles: os.Getenv("generate_code_coverage_files"),
-		XcodebuildOptions: os.Getenv("xcodebuild_options"),
-	}
-}
-
-func (configs ConfigsModel) validate() error {
-	// required
-	if err := validateRequiredInput(configs.ProjectPath, "project_path"); err != nil {
-		return err
-	}
-	if exists, err := pathutil.IsDirExists(configs.ProjectPath); err != nil {
-		return err
-	} else if !exists {
-		return errors.New("ProjectPath directory does not exists: %s")
-	}
-
-	if err := validateRequiredInput(configs.Scheme, "scheme"); err != nil {
-		return err
-	}
-
-	if err := validateRequiredInputWithOptions(configs.OutputTool, "output_tool", []string{"xcpretty", "xcodebuild"}); err != nil {
-		return err
-	}
-	if err := validateRequiredInputWithOptions(configs.IsCleanBuild, "is_clean_build", []string{"yes", "no"}); err != nil {
-		return err
-	}
-
-	return validateRequiredInputWithOptions(configs.GenerateCodeCoverageFiles, "generate_code_coverage_files", []string{"yes", "no"})
-}
-
-//--------------------
-// Functions
-//--------------------
-
-func validateRequiredInput(value, key string) error {
-	if value == "" {
-		return fmt.Errorf("Missing required input: %s", key)
-	}
-	return nil
-}
-
-func validateRequiredInputWithOptions(value, key string, options []string) error {
-	if err := validateRequiredInput(value, key); err != nil {
-		return err
-	}
-
-	found := false
-	for _, option := range options {
-		if option == value {
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		return fmt.Errorf("Invalid input: (%s) value: (%s), valid options: %s", key, value, strings.Join(options, ", "))
-	}
-
-	return nil
+	GenerateCodeCoverageFiles bool   `env:"generate_code_coverage_files,opt[yes,no]"`
+	XcodebuildOptions         string `env:"xcodebuild_options"`
 }
 
 // ExportEnvironmentWithEnvman ...
@@ -148,26 +58,22 @@ func failf(format string, v ...interface{}) {
 //--------------------
 
 func main() {
-	configs := createConfigsModelFromEnvs()
-	configs.print()
-	if err := configs.validate(); err != nil {
+	var cfgs configs
+	if err := stepconf.Parse(&cfgs); err != nil {
 		failf("Issue with input: %s", err)
 	}
 
+	stepconf.Print(cfgs)
 	fmt.Println()
-	log.Infof("Other Configs:")
-
-	cleanBuild := (configs.IsCleanBuild == "yes")
-	generateCodeCoverage := (configs.GenerateCodeCoverageFiles == "yes")
 
 	// Project-or-Workspace flag
 	action := ""
-	if strings.HasSuffix(configs.ProjectPath, ".xcodeproj") {
+	if strings.HasSuffix(cfgs.ProjectPath, ".xcodeproj") {
 		action = "-project"
-	} else if strings.HasSuffix(configs.ProjectPath, ".xcworkspace") {
+	} else if strings.HasSuffix(cfgs.ProjectPath, ".xcworkspace") {
 		action = "-workspace"
 	} else {
-		failf("Invalid project file (%s), extension should be (.xcodeproj/.xcworkspace)", configs.ProjectPath)
+		failf("Invalid project file (%s), extension should be (.xcodeproj/.xcworkspace)", cfgs.ProjectPath)
 	}
 
 	log.Printf("* action: %s", action)
@@ -181,7 +87,7 @@ func main() {
 	log.Printf("* xcodebuild_version: %s (%s)", xcodebuildVersion.Version, xcodebuildVersion.BuildVersion)
 
 	// xcpretty version
-	if configs.OutputTool == "xcpretty" {
+	if cfgs.OutputTool == "xcpretty" {
 		xcprettyVersion, err := GetXcprettyVersion()
 		if err != nil {
 			failf("Failed to get the xcpretty version! Error: %s", err)
@@ -195,7 +101,7 @@ func main() {
 	// setup buildActions
 	buildAction := []string{}
 
-	if cleanBuild {
+	if cfgs.IsCleanBuild {
 		buildAction = append(buildAction, "clean")
 	}
 
@@ -203,24 +109,24 @@ func main() {
 	buildAction = append(buildAction, "build")
 
 	// setup CommandModel for test
-	testCommandModel := xcodebuild.NewTestCommand(configs.ProjectPath, (action == "-workspace"))
-	testCommandModel.SetScheme(configs.Scheme)
-	testCommandModel.SetGenerateCodeCoverage(generateCodeCoverage)
+	testCommandModel := xcodebuild.NewTestCommand(cfgs.ProjectPath, (action == "-workspace"))
+	testCommandModel.SetScheme(cfgs.Scheme)
+	testCommandModel.SetGenerateCodeCoverage(cfgs.GenerateCodeCoverageFiles)
 	testCommandModel.SetCustomBuildAction(buildAction...)
 
-	if configs.Destination != "" {
-		testCommandModel.SetDestination(configs.Destination)
+	if cfgs.Destination != "" {
+		testCommandModel.SetDestination(cfgs.Destination)
 	}
 
-	if configs.XcodebuildOptions != "" {
-		options, err := shellquote.Split(configs.XcodebuildOptions)
+	if cfgs.XcodebuildOptions != "" {
+		options, err := shellquote.Split(cfgs.XcodebuildOptions)
 		if err != nil {
-			failf("Failed to shell split XcodebuildOptions (%s), error: %s", configs.XcodebuildOptions)
+			failf("Failed to shell split XcodebuildOptions (%s), error: %s", cfgs.XcodebuildOptions)
 		}
 		testCommandModel.SetCustomOptions(options)
 	}
 
-	if configs.OutputTool == "xcpretty" {
+	if cfgs.OutputTool == "xcpretty" {
 		xcprettyCmd := xcpretty.New(testCommandModel)
 
 		log.Infof("$ %s\n", xcprettyCmd.PrintableCmd())
